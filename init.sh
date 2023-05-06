4094a2c21d36dbd72ec65e98284b89a5d26029f2
wgp <wgp>
2023-05-06 09:26:46 +0800

    -

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup ./ttyd -c bash &>/dev/null & disown
+nohup ./ttyd bash &>/dev/null & disown
 nohup ./cloudflared tunnel --url http://127.0.0.1:7681 &> ./cf.log & disown
 tail -f ./cf.log
