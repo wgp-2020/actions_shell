c5de00a2867498bea57975008dbcb6caa9f34221
wgp-2020 <70625361+wgp-2020@users.noreply.github.com>
2023-05-06 09:28:46 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 nohup ./ttyd bash &>/dev/null & disown
+echo $(ps aux)
+echo $(ss -tulp)
 nohup ./cloudflared tunnel --url http://127.0.0.1:7681 &> ./cf.log & disown
 tail -f ./cf.log
